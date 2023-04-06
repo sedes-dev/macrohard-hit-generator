5194a0660aa3dabd1ceaf3d03540f15a04512c48
mihauco <github@mihau.co>
2023-04-06 15:35:30 +0200

    last few updates before going live with this crap

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ For now only polish version is available.
 ## Development
 
 Project uses react and was bootstraped with Create React App + typescript + redux.
+
+## Credits
+
+Made with 💩 (and react) by [sedes.dev](https://sedes.dev).
+
+Nice retro style provided by [98.css](https://jdan.github.io/98.css/)
+
+## Spotify playlist
+
+If you're kind of a boomer and preffer old-style polish rap, here's a [spotify playlist](https://open.spotify.com/playlist/4mTbaoYXTfqrAjn47j11GY?si=c0ed181f30ca43f2) for you.
+
--- src/components/HelpWindow.tsx
+++ src/components/HelpWindow.tsx
@@ -13,8 +13,13 @@ export function HelpWindow(props: HelpWindowProps) {
     <Window title="Info" initialSize={{width: 300}} onClose={props.onClose} center=".window--main">
       <p><strong>Macrohard Hit Generator v{packageJson.version}</strong></p>
       <hr className="help-window__divider"/>
-      <p>Ilość kawałków w bazie: {tracks.length}</p>
-      <p>Made with 💩 by <a href="https://sedes.dev" target="_blank" rel="noreferrer">sedes.dev</a></p>
+      <br/>
+      <p className="help-window__text">
+        Made with 💩 by <a href="https://sedes.dev" target="_blank" rel="noreferrer">sedes.dev</a><br/>
+        Ilość kawałków w bazie: {tracks.length}<br/>
+        Playlista na <a href="https://open.spotify.com/playlist/4mTbaoYXTfqrAjn47j11GY?si=b3efe4f7cedc4cb4" target="_blank" rel="noreferrer">Spotify</a><br/>
+        Retro styl <a href="https://jdan.github.io/98.css/" target="_blank" rel="noreferrer">98.css</a>
+      </p>
     </Window>
   )
 }
